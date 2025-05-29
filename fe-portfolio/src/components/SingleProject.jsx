81f9640d9b9ae5fa7e434db7af3c401c3eab1850
Liv Motley <livmotley.uk@gmail.com>
2025-05-29 17:58:48 +0100

    completed carousel for project page

--- fe-portfolio/src/components/SingleProject.jsx
+++ fe-portfolio/src/components/SingleProject.jsx
@@ -1,14 +1,17 @@
-import { Link, useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import { getProjectByTitle } from "../api";
 import { useEffect, useState } from "react";
 import "../unique-css/Single-Project.css";
 import ImageCarousel from "./ImageCarousel";
+import { IoIosArrowBack } from "react-icons/io";
+
 
 function SingleProject() {
     const { title } = useParams();
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
     const [project, setProject] = useState(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
                 setIsLoading(true);
@@ -31,11 +34,14 @@ function SingleProject() {
     if(isError) return <p className="error-container">Sorry! Error loading the project. Please refresh.</p>
     if (!project) return <p className="error-container">Project not found</p >;
 
-console.log(project);
+
     return (
         <div className="single-project-container">
             <div className="project-header">
-                <h2 className="page-title">{project.title}</h2>
+                <div className="title-section">
+                    <button className="back-button" onClick={(() => navigate(-1))}><IoIosArrowBack className="back-icon"/> All Projects</button>
+                    <h2 className="page-title">{project.title}</h2>
+                </div>
                 <div className="tech-stack-container">
                     {project.tech_stack.map((tech) => {
                         return <p key={tech} className="tech-name">{tech}</p>
@@ -44,10 +50,10 @@ console.log(project);
             </div>
             <div className="project-content">
                 <div className="project-assets">
-                    <ImageCarousel images={project.assets}/>
+                    <ImageCarousel assets={project.assets}/>
                 </div>
                 <div className="project-details">
-                    <p className="page-description">{project.description}</p>
+                    <p className="project-description">{project.description}</p>
                     {project.github_link_two ? (
                         <div className="github-links-container">
                             <Link to={project.github_link_one} className="github-link">GitHub Link: BE</Link>
